faa5f3b292d04040beb144056ef8c797dc65a4a5
Diego Câmara <diegoluizjava.java@gmail.com>
2019-09-02 08:36:06 -0300

    fix tests

--- src/test/java/org/example/realworldapi/domain/repository/UsersRepositoryImplTest.java
+++ src/test/java/org/example/realworldapi/domain/repository/UsersRepositoryImplTest.java
@@ -1,21 +1,12 @@
 package org.example.realworldapi.domain.repository;
 
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.h2.H2DatabaseTestResource;
+import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-
-@QuarkusTestResource(H2DatabaseTestResource.class)
+@QuarkusTest
+//@QuarkusTestResource(H2DatabaseTestResource.class)
 public class UsersRepositoryImplTest {
-
-    @Inject
-    EntityManager entityManager;
-
     @Test
     public void testing() {
-        entityManager.clear();
     }
-
 }
