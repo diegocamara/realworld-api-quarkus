4a7b94c7c0efc323a5f6fe655fa5f5c40e93b88a
diegocamara <diegoluiz.cjava@gmail.com>
2022-06-12 22:37:16 -0300

    Fix integrated tests run

--- integrated-tests.sh
+++ integrated-tests.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-java -jar target/quarkus-app/quarkus-run.jar
+java -jar target/quarkus-app/quarkus-run.jar > service.log &
+SERVICE_PROCESS=$!
+tail -f -n0 service.log | grep -q 'Listening on'
+echo "Application started"
+./collections/run-api-tests.sh
+kill $SERVICE_PROCESS
+rm service.log
 
