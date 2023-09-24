120a2e6e2d07c42743aeeed2764f4c78b6e974b8
asjervanasten <asjer94@live.com>
2023-09-24 19:16:19 +0200

    Added Postman integration tests

--- src/main/java/org/example/realworldapi/infrastructure/configuration/ApplicationConfiguration.java
+++ src/main/java/org/example/realworldapi/infrastructure/configuration/ApplicationConfiguration.java
@@ -5,35 +5,35 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.slugify.Slugify;
-import org.example.realworldapi.infrastructure.web.qualifiers.NoWrapRootValueObjectMapper;
-
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Singleton;
+import org.example.realworldapi.infrastructure.web.qualifiers.NoWrapRootValueObjectMapper;
 
 @Dependent
 public class ApplicationConfiguration {
 
-  @Produces
-  @Singleton
-  public Slugify slugify() {
-    return new Slugify();
-  }
+    @Produces
+    @Singleton
+    public Slugify slugify() {
+        Slugify slug = Slugify.builder().build();
+        return slug;
+    }
 
-  @Singleton
-  @Produces
-  public ObjectMapper objectMapper() {
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
-    objectMapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
-    objectMapper.registerModule(new JavaTimeModule());
-    return objectMapper;
-  }
+    @Singleton
+    @Produces
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        objectMapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
 
-  @Singleton
-  @Produces
-  @NoWrapRootValueObjectMapper
-  public ObjectMapper noWrapRootValueObjectMapper() {
-    return new ObjectMapper().registerModule(new JavaTimeModule());
-  }
+    @Singleton
+    @Produces
+    @NoWrapRootValueObjectMapper
+    public ObjectMapper noWrapRootValueObjectMapper() {
+        return new ObjectMapper().registerModule(new JavaTimeModule());
+    }
 }
